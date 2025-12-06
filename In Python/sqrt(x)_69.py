7ece2d8eead0fd19e21108af1f08a4a5f810bcb9
Manthan Terse <tersemanthan2006@gmail.com>
2025-12-06 17:03:00 +0530

    Implement binary search for square root calculation
    Refactor mySqrt method to use binary search for square root calculation.

--- In Python/sqrt(x)_69.py
+++ In Python/sqrt(x)_69.py
@@ -1,4 +1,15 @@
 class Solution(object):
     def mySqrt(self, x):
-        y = int(math.sqrt(x))
-        return y
+        if x < 2:
+            return x
+        
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
